14f76b4b67aac87b09b1b99d36d0aae00046e071
krunom01 <krunom92@gmail.com>
2019-01-24 03:36:44 +0100

    function start

---
+++ funkcija.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*  $k - pocetak reda
+        $m - kraj reda
+        $l - pocetak stupca
+        $n - kraj stupca
+        $i - okidač
+
+    */
+
+ 
+function spiralPrint($m, $n, &$a) // koristenje pointera unutar funkcije kako bi $a i izvan funkcije imao novu vrijednost 
+
+
+
+ 
+
